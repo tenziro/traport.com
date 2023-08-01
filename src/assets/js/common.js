3bfab4287951acd9d528ed57dc17518324e5ae94
tenziro <tenziro@naver.com>
2023-08-01 15:15:11 +0900

    다크모드 적용, favicon 적용

--- src/assets/js/common.js
+++ src/assets/js/common.js
@@ -1,50 +1,52 @@
 function scrollDim() {
-  var maxDarkness = 0.9;
-  var minDarkness = 0.6;
-  var maxScroll = $("#visual").height();
+  const maxDarkness = 0.9;
+  const minDarkness = 0.6;
+  const maxScroll = $("#visual").height();
 
   $(window).scroll(function () {
-    var currentScroll = $(this).scrollTop();
+    const currentScroll = $(this).scrollTop();
 
     if (currentScroll <= maxScroll) {
-      var darkness = (currentScroll / maxScroll) * (maxDarkness - minDarkness) + minDarkness;
-      $('#visual .dim').css('background-color', 'rgba(0, 0, 0, ' + darkness + ')');
+      const darkness = (currentScroll / maxScroll) * (maxDarkness - minDarkness) + minDarkness;
+      $('#visual .dim').css('background-color', `rgba(0, 0, 0, ${darkness})`);
     } else {
-      $('#visual .dim').css('background-color', 'rgba(0, 0, 0, ' + maxDarkness + ')');
+      $('#visual .dim').css('background-color', `rgba(0, 0, 0, ${maxDarkness})`);
     }
   });
 }
 
-var eventVisual = {
+const eventVisual = {
   init: function () {
-    var visualTitle = $('#visual .title');
+    const visualTitle = $('#visual .title');
     setTimeout(function () {
       visualTitle.addClass('animate-start');
-    }, 1500)
+    }, 1500);
   }
 }
 
 $(document).ready(function () {
-  AOS.init({
-    once: false,
-    duration: 900,
-  });
-
   eventVisual.init();
 
+  // AOS 설정
+  AOS.init({
+    once: true,
+    duration: 1000,
+  });
+
+  // nav 설정
   $(".nav-item a").click(function (event) {
     event.preventDefault();
-    var target = $($(this).attr("href"));
+    const target = $($(this).attr("href"));
     $("html, body").animate({
       scrollTop: target.offset().top
     }, 300);
   });
 
   function updateActiveNavItem() {
-    var scrollPos = $(document).scrollTop();
+    const scrollPos = $(document).scrollTop();
     $(".nav-item a").each(function () {
-      var currLink = $(this);
-      var refElement = $(currLink.attr("href"));
+      const currLink = $(this);
+      const refElement = $(currLink.attr("href"));
       if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
         $(".nav-item a").removeClass("focus");
         currLink.addClass("focus");
@@ -53,23 +55,54 @@ $(document).ready(function () {
       }
     });
   }
+
   function checkFooterVisibility() {
-    var footer = $("#footer");
+    const footer = $("#footer");
     if (isScrolledIntoView(footer)) {
       $(".nav-item a").removeClass("focus");
     }
   }
+
   function isScrolledIntoView(elem) {
-    var docViewTop = $(window).scrollTop();
-    var docViewBottom = docViewTop + $(window).height();
-    var elemTop = $(elem).offset().top;
-    var elemBottom = elemTop + $(elem).height();
+    const docViewTop = $(window).scrollTop();
+    const docViewBottom = docViewTop + $(window).height();
+    const elemTop = $(elem).offset().top;
+    const elemBottom = elemTop + $(elem).height();
     return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
   }
   $(document).on("scroll", updateActiveNavItem);
   $(document).on("scroll", checkFooterVisibility);
 
   scrollDim();
+
+  let currentIndex = 0;
+  const $backgroundItems = $('.visual-item');
+
+  function showNextBackground() {
+    $backgroundItems.stop().fadeTo(1200, 0, function () {
+      let targetBg = $(this);
+      setTimeout(function () {
+        targetBg.removeClass('scale');
+      }, 1000)
+    });
+
+    currentIndex = getRandomIndex(currentIndex, 3);
+    $backgroundItems.eq(currentIndex).stop().fadeTo(1200, 1, function () {
+      $(this).addClass('scale');
+    });
+
+    setTimeout(showNextBackground, 10000);
+  }
+
+  function getRandomIndex(currentIndex, maxIndex) {
+    let randomIndex = currentIndex;
+    while (randomIndex === currentIndex) {
+      randomIndex = Math.floor(Math.random() * maxIndex);
+    }
+    return randomIndex;
+  }
+
+  showNextBackground();
 });
 
 $(document).resize(function () {
